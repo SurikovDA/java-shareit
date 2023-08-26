33f61b40fb6432662217cf2e26c255dcdcd351ff
Dmitrii <SurikovDA@yandex.ru>
2023-08-27 01:32:32 +0300

    15 спринт. 1 проверка

--- src/test/java/ru/practicum/shareit/bookingTests/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/bookingTests/BookingRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
 import ru.practicum.shareit.booking.Status;
 import ru.practicum.shareit.booking.model.Booking;
@@ -16,7 +17,6 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.storage.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -49,13 +49,13 @@ class BookingRepositoryTest {
                 .email("user1@mail.ru")
                 .build());
         request = requestRepository.save(ItemRequest.builder()
-                .id(2L)
+                .id(1L)
                 .description("description")
                 .requestor(user)
                 .created(LocalDateTime.now())
                 .build());
         item = itemRepository.save(Item.builder()
-                .id(3L)
+                .id(1L)
                 .name("item1")
                 .description("description1")
                 .available(true)
@@ -63,7 +63,7 @@ class BookingRepositoryTest {
                 .itemRequest(request)
                 .build());
         booking = bookingRepository.save(Booking.builder()
-                .id(4L)
+                .id(1L)
                 .booker(user)
                 .status(Status.APPROVED)
                 .item(item)
@@ -76,89 +76,110 @@ class BookingRepositoryTest {
 
     @Test
     void findAllByBookerIdOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByBookerIdOrderByStartDesc(1L, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(1L,
+                end, start, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByBookerIdAndEndBeforeOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByBookerIdAndEndBeforeOrderByStartDesc(1L,
+                end, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByBookerIdAndStartAfterOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByBookerIdAndStartAfterOrderByStartDesc(1L,
+                start, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByBookerIdAndStatusOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByBookerIdAndStatusOrderByStartDesc(1L,
+                Status.APPROVED, Pageable.unpaged()).toList();
+        List<Booking> bookings2 = bookingRepository.findAllByBookerIdAndStatusOrderByStartDesc(1L,
+                Status.WAITING, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
+        assertEquals(bookings2.size(), 0);
     }
 
     @Test
     void findAllByItem_OwnerIdOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByItem_OwnerIdOrderByStartDesc(1L,
+                Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByItem_OwnerIdAndStartBeforeAndEndAfterOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByItem_OwnerIdAndStartBeforeAndEndAfterOrderByStartDesc(1L,
+                end, start, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByItem_OwnerIdAndEndBeforeOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByItem_OwnerIdAndEndBeforeOrderByStartDesc(1L,
+                end, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByItem_OwnerIdAndStartAfterOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByItem_OwnerIdAndStartAfterOrderByStartDesc(1L,
+                start, Pageable.unpaged()).toList();
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
     void findAllByItem_OwnerIdAndStatusOrderByStartDesc() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+        List<Booking> bookings = bookingRepository.findAllByItem_OwnerIdAndStatusOrderByStartDesc(1L,
+                Status.APPROVED, Pageable.unpaged()).toList();
+        List<Booking> bookings2 = bookingRepository.findAllByItem_OwnerIdAndStatusOrderByStartDesc(1L,
+                Status.WAITING, Pageable.unpaged()).toList();
+
+        assertEquals(bookings.size(), 1);
+        assertEquals(bookings2.size(), 0);
+    }
+
+    @Test
+    void findAllByItemIdAndAndBooker_IdAndEndBefore() {
+        List<Booking> bookings = bookingRepository.findAllByItemIdAndAndBooker_IdAndEndBefore(1L,
+                1L, end);
 
         assertEquals(bookings.size(), 1);
     }
 
     @Test
-    void findAllByItemIdAndAndBooker_IdAndEndBefore() {
-        List<Booking> bookings = new ArrayList<>();
-        bookings.add(booking);
+    void findByItemAndEndBeforeOrderByEndDesc() {
+        Booking booking1 = bookingRepository.findFirstByItemIdAndStartBeforeAndStatusOrderByStartDesc(item.getId(),
+                end, Status.APPROVED);
 
-        assertEquals(bookings.size(), 1);
+        assertEquals(booking1.getItem(), item);
+    }
+
+    @Test
+    void findByItemAndStartAfterOrderByStart() {
+        Booking booking1 = bookingRepository.findFirstByItemAndStartAfterAndStatusOrderByStart(item, start,
+                Status.APPROVED);
+
+        assertEquals(booking1.getItem(), item);
     }
 }
