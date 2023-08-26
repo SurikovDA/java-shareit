580312c968e91c35513656dcfd9030e66512d084
Dmitrii <SurikovDA@yandex.ru>
2023-08-26 05:41:04 +0300

    15 спринт. 1 проверка

--- src/test/java/ru/practicum/shareit/bookingTests/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/bookingTests/BookingRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
 import ru.practicum.shareit.booking.Status;
 import ru.practicum.shareit.booking.model.Booking;
