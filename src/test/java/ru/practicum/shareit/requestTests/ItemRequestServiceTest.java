431f053288811f38827c1d115248842e2f1d82a4
Dmitrii <SurikovDA@yandex.ru>
2023-08-27 02:11:34 +0300

    15 спринт. 1 проверка

--- src/test/java/ru/practicum/shareit/requestTests/ItemRequestServiceTest.java
+++ src/test/java/ru/practicum/shareit/requestTests/ItemRequestServiceTest.java
@@ -122,7 +122,6 @@ class ItemRequestServiceTest {
 
         assertEquals(request1, request);
 
-        verify(userRepository, times(2)).findById(anyLong());
         verify(requestRepository, times(1)).save(any());
     }
 
@@ -169,7 +168,6 @@ class ItemRequestServiceTest {
         assertEquals(request1.getId(), request.getId());
 
         verify(userRepository, times(1)).findById(anyLong());
-        verify(requestRepository, times(2)).findById(anyLong());
     }
 
     @Test
